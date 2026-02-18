2c3708a2c6c9e5331264fb6bc18e2d97f42cfaa3
Ayoub Azizi <axa200170@utdallas.edu>
2026-02-18 03:27:34 -0600

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById('root');
+
+// This check prevents the "implicitly has type any" and "possibly null" errors
+if (!rootElement) {
+  throw new Error('Failed to find the root element. Ensure index.html has <div id="root"></div>');
+}
+
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
